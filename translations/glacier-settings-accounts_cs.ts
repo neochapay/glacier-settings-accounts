a680d2d8cd9699fde51e1ee42300faa8075cc123
Jozef Mlich <jmlich83@gmail.com>
2021-12-21 17:23:39 +0000

    install jabber provider as well

---
+++ translations/glacier-settings-accounts_cs.ts
@@ -0,0 +1,46 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
+<context>
+    <name>CreateAccountSheet</name>
+    <message>
+        <location filename="../qml/plugins/accounts/CreateAccountSheet.qml" line="42"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>accounts</name>
+    <message>
+        <location filename="../qml/plugins/accounts/accounts.qml" line="42"/>
+        <source>Accounts</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>email</name>
+    <message>
+        <location filename="../qml/plugins/accounts/providers/email.qml" line="44"/>
+        <source>Email</source>
+        <translation>Email</translation>
+    </message>
+    <message>
+        <location filename="../qml/plugins/accounts/providers/email.qml" line="320"/>
+        <source>Save</source>
+        <translation>Uložit</translation>
+    </message>
+</context>
+<context>
+    <name>jabber</name>
+    <message>
+        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="43"/>
+        <source>Jabber</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/plugins/accounts/providers/jabber.qml" line="152"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
